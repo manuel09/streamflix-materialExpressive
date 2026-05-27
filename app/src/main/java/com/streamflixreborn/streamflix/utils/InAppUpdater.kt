039c935b6385c8d058fa5f5ad409538f7ed5fe76
manuel09 <manu.17@live.it>
2026-05-27 16:25:12 +0200

    Aggiornamento puntamento updater al repository personale

--- app/src/main/java/com/streamflixreborn/streamflix/utils/InAppUpdater.kt
+++ app/src/main/java/com/streamflixreborn/streamflix/utils/InAppUpdater.kt
@@ -14,8 +14,8 @@ import kotlin.math.max
 
 object InAppUpdater {
 
-    private const val GITHUB_OWNER = "streamflix-reborn"
-    private const val GITHUB_REPO = "streamflix"
+    private const val GITHUB_OWNER = "manuel09"
+    private const val GITHUB_REPO = "streamflix-materialExpressive"
 
     private data class Version(val name: String) : Comparable<Version> {
         override operator fun compareTo(other: Version): Int {
